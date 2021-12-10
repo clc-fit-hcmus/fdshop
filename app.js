52fa9ae4fe673456d027c14647e254d52abe8f02
hip-nncuong162 <63356009+hip-nncuong162@users.noreply.github.com>
2021-12-10 16:38:24 +0700

    update port

--- app.js
+++ app.js
@@ -121,10 +121,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 8080;
-
-app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 module.exports = app;
