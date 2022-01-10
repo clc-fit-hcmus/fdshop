628550a669859e49369d270c018276c6816a3c0e
HiImLen <lopquay453@gmail.com>
2022-01-10 17:31:28 +0700

    Add book table/reservation

--- components/reservations/index.js
+++ components/reservations/index.js
@@ -3,8 +3,12 @@ const {postReservation, getReservations, getReservation} = require("./reservatio
 
 const router = express.Router();
 
-router.get('/', getReservations);
-router.get('/:phone', getReservation);
-router.post('/add', postReservation);
+router.get('/reservation', function(req, res, next) {
+    const successMessages = req.flash('success');
+    const errorMessages = req.flash('error');
+    res.render('reservation/reservation', { errorMessages: errorMessages, hasErrors: errorMessages.length > 0, successMessages: successMessages, hasSuccesses: successMessages.length > 0 });
+  });
+  
+router.post('/reservation', postReservation);
 
 module.exports = router;
--- components/reservations/reservationsService.js
+++ components/reservations/reservationsService.js
@@ -4,6 +4,11 @@ const query = (filter = {}) => Reservation.find(filter);
 
 // add Reservation to DB
 const save = async (req) => {
+    if (!/^\d+$/.test(req.body.phone_number)) {
+        req.flash('error', 'Your phone must contains only digits!');
+        return;
+    }
+
     const {phone_number} = req.body;
     const {peoples} = req.body;
     const {when} = req.body;
@@ -14,7 +19,8 @@ const save = async (req) => {
         when: when
     });
 
-    return await newReservation.save();
+    req.flash('success', 'You have successfully booked a table!');
+    await newReservation.save();
 };
 
 module.exports = {
